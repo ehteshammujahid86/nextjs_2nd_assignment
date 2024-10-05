acb5d8c10e0553a6e6a97ac948e05cafaecee917
ehteshammujahid86 <ehteshammujahid86@gmail.com>
2024-10-05 13:06:02 +0500

    first commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <div className="header">
+            <ul className="nav_items">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Jobs</li>
+            </ul>
+        </div>
+    )
+}
